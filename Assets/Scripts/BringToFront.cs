7abb5e507a0c8337633ae4f87a81cfc0dedf525d
Tanay Nigam <tanaynigam12@gmail.com>
2020-02-29 10:15:56 -0500

    First Commit

---
+++ Assets/Scripts/BringToFront.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BringToFront : MonoBehaviour
+{
+
+    public float distance;
+    public GameObject banner;
+    public Text bannerText; 
+
+
+    private bool isObjectInFront;
+    private GameObject objectInFront;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isObjectInFront = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                if (isObjectInFront)
+                {
+                    objectInFront.transform.position = originalPosition;
+                    objectInFront.transform.rotation = originalRotation;
+                    isObjectInFront = false;
+                    banner.SetActive(false);
+                }
+                else
+                {
+                    Touch touch = Input.GetTouch(0);
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                    if (Physics.Raycast(ray, out hit, 2000))
+                    {
+                        if (hit.transform.tag == "Frames")
+                        {
+                            originalPosition = hit.transform.position;
+                            originalRotation = hit.transform.rotation;
+                            hit.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance;
+                            hit.transform.LookAt(Camera.main.transform, hit.transform.up);
+                            objectInFront = hit.transform.gameObject;
+                            banner.SetActive(true);
+                            bannerText.text = hit.transform.gameObject.name;
+                        }
+                    }
+                    isObjectInFront = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/ImageAnchorManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageAnchorManager : MonoBehaviour
+{
+    public GameObject Experience;
+    public float distance;
+    private Vector3 imagePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject anchor = GameObject.FindWithTag("Anchor");
+        imagePosition = anchor.transform.position;
+        if (anchor != null && Vector3.Distance(Camera.main.transform.position, imagePosition) < distance)
+        {
+            Experience.SetActive(true);
+            Experience.transform.position = anchor.transform.position;
+            Experience.transform.rotation = anchor.transform.rotation;
+            Experience.transform.localScale = anchor.transform.localScale;
+            imagePosition = Experience.transform.position;
+        }
+
+        if(Vector3.Distance(Camera.main.transform.position, imagePosition) > distance)
+        {
+            Experience.SetActive(false);
+        }
+    }
+}
